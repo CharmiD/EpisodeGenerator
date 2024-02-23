8f92df7a66cda8dc34dd53ba5033299ca52117fc
CharmiD <43147080+CharmiD@users.noreply.github.com>
2024-02-23 15:32:30 -0500

    Converted to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "./index.css";
@@ -11,9 +11,9 @@ import store from "./App/Store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
   </Provider>
 );
 
